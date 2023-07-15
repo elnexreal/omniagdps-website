f95371959c956fbe26158fa3aa13d524f2d68f2d
Axel Pundang <lolopundang1@gmail.com>
2023-07-15 03:12:12 -0300

    added base design

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+	static: path.resolve(__dirname, 'public'),
+    compress: true,
+    port: 8080,
+  },
+};
